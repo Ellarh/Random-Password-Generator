d0a03978542dd8e42387b041a8c962e7e8510758
Emmanuella Ndukwe-Obiukwu <ellamoves01@gmail.com>
2022-06-06 22:13:29 +0000

    Added a way to ask the user if they want to generate another password or not, and stop the program if they wish not to continue.

--- main.py
+++ main.py
@@ -4,26 +4,35 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+will_continue = True
 
+while will_continue:
+  nr_letters= int(input("How many letters would you like in your password?\n")) 
+  nr_symbols = int(input(f"How many symbols would you like?\n"))
+  nr_numbers = int(input(f"How many numbers would you like?\n"))
+  
+  
+  password_list = []
+  
+  for char in range(1, nr_letters + 1):
+    password_list += random.choice(letters)
+  
+  for char in range(1, nr_symbols + 1):
+    password_list += random.choice(numbers)
+  
+  for char in range(1, nr_numbers + 1):
+    password_list += random.choice(symbols)
+  
+  random.shuffle(password_list)
+  
+  password = ""
+  for char in password_list:
+    password += char
+  
+  print(f"Your generated password is: {password}")
 
-password_list = []
-
-for char in range(1, nr_letters + 1):
-  password_list += random.choice(letters)
-
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(numbers)
-
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(symbols)
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your generated password is: {password}")
+  decision = input("Do you want to get another passoword? Type 'yes' or 'no' ")
+  
+  if decision == "no":
+    will_continue = False
+    print("You are done. Thank you")
